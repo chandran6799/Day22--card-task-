0fea34d6b8be25aab10777cd94db38ed9cefef1e
chandran6799 <chandranak06071999@gmail.comgit config --global user.email chandranak06071999@gmail.com>
2024-05-10 03:54:17 -0700

    Price card Commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import "./App.css";
+import Card from "./components/Card";
+
+const card = [
+  {
+    title: "FREE",
+    price: "$0",
+    user: "Single User",
+    storage: "50GB Storage",
+    domain: "Free Domain",
+    project: "text-muted",
+    projectIcon: "fas fa-times",
+    phone: "text-muted",
+    phoneIcon: "fas fa-times",
+    subDomain: "text-muted",
+    subDomainIcon: "fas fa-times",
+    monthly: "text-muted",
+    monthlyIcon: "fa fa-times",
+  },
+  {
+    title: "PLUS",
+    price: "$9",
+    user: "5 Users",
+    storage: "50GB Storage",
+    domain: "Free Domain",
+    project: "",
+    projectIcon: "fas fa-check",
+    phone: "",
+    phoneIcon: "fas fa-check",
+    subDomain: "",
+    subDomainIcon: "fas fa-check",
+    monthly: "text-muted",
+    monthlyIcon: "fa fa-times",
+  },
+  {
+    title: "PRO",
+    price: "$49",
+    user: "Unlimited Users",
+    storage: "50GB Storage",
+    domain: "Un",
+    project: "",
+    projectIcon: "fas fa-check",
+    phone: "",
+    phoneIcon: "fas fa-check",
+    subDomain: "",
+    subDomainIcon: "fas fa-check",
+    monthly: "",
+    monthlyIcon: "fa fa-check",
+  },
+];
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container bg-primary h-100 my-5 p-4">
+        <div className="row">
+          {card.map((element, index) => {
+            return (
+              <>
+                <Card key={index} cardData={element} />
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
